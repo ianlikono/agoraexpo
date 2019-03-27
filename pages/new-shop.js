7a03b867daca77b0ab08f600be761387e285bb4a
Ian <ian@Deans-MacBook-Pro.local>
2019-03-27 09:48:14 -0500

    transfer

--- pages/new-shop.js
+++ pages/new-shop.js
@@ -1,8 +1,24 @@
 import Button from '@material-ui/core/Button';
+import green from '@material-ui/core/colors/green';
+import IconButton from '@material-ui/core/IconButton';
 import Paper from '@material-ui/core/Paper';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
 import withStyles from '@material-ui/core/styles/withStyles';
 import TextField from '@material-ui/core/TextField';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import CloseIcon from '@material-ui/icons/Close';
+import ErrorIcon from '@material-ui/icons/Error';
+import classNames from 'classnames';
+import gql from 'graphql-tag';
 import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import { createShop } from '../src/graphql/mutations';
+
+const variantIcon = {
+  success: CheckCircleIcon,
+  error: ErrorIcon,
+};
 
 const styles = theme => ({
   paper: {
@@ -12,78 +28,144 @@ const styles = theme => ({
     alignItems: 'center',
     padding: `${theme.spacing.unit * 5}px ${theme.spacing.unit * 5}px ${theme.spacing.unit * 5}px`,
   },
-  container: {
-    maxWidth: '200px',
+  error: {
+    backgroundColor: theme.palette.error.dark,
+  },
+  success: {
+    backgroundColor: green[600],
+  },
+  icon: {
+    fontSize: 20,
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing.unit,
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center',
   },
 });
 
-class CreateShop extends Component {
+class CreateShopPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       name: '',
       category: '',
       description: '',
+      open: true,
+      successSnackBar: false,
+      errorSnackBar: false,
     };
   }
+
+  renderSnackBar = (snackItems, status) => {
+    const { classes, variant , className, other, onClose, message } = snackItems;
+    const Icon = variantIcon[variant];
+    if(status === 'success') {
+      variant = 'success'
+      message = 'this is a success message'
+    }
+    return (
+      <SnackbarContent
+        className={classNames(classes[variant], className)}
+        aria-describedby="client-snackbar"
+        message={
+          <span id="client-snackbar" className={classes.message}>
+            <Icon className={classNames(classes.icon, classes.iconVariant)} />
+            {message}
+          </span>
+        }
+        action={[
+          <IconButton
+            key="close"
+            aria-label="Close"
+            color="inherit"
+            className={classes.close}
+            onClick={onClose}
+          >
+            <CloseIcon className={classes.icon} />
+          </IconButton>,
+        ]}
+        {...other}
+      />
+    );
+  };
 
   handleInputChange = e => {
     const { name, value } = e.target;
     this.setState({ [name]: value });
   };
 
-  formSubmit = () => {
-    console.log(this.state);
+  formSubmit = (func, error) => {
+    console.log(func);
+    console.log(error);
   };
 
   render() {
-    const classes = this.props;
-    const { name, category, description } = this.state;
+    const { name, category, description, open } = this.state;
+    const { classes, className, message, onClose, variant, ...other } = this.props;
+    const snackItems = { classes, variant, className, other, onClose, message };
     return (
-      <React.Fragment>
-        <div style={{ maxWidth: '600px', margin: '0 auto' }}>
-          <h1 style={{ textAlign: 'center' }}>Create Shop</h1>
-          <Paper elevation={1} className={classes.paper}>
-            <TextField
-              onChange={this.handleInputChange}
-              id="name"
-              name="name"
-              label="Shop Name"
-              fullWidth
-              value={name}
-            />
-            <TextField
-              onChange={this.handleInputChange}
-              id="category"
-              name="category"
-              label="Category"
-              fullWidth
-              value={category}
-            />
-            <TextField
-              onChange={this.handleInputChange}
-              id="description"
-              name="description"
-              label="Description"
-              fullWidth
-              rows={5}
-              multiline
-              value={description}
-            />
-            <Button
-              onClick={this.formSubmit}
-              type="submit"
-              fullWidth
-              variant="raised"
-              color="primary"
+      <Mutation mutation={gql(createShop)}>
+        {(createShopFunction, { loading, error }) => (
+          <div style={{ maxWidth: '600px', margin: '0 auto' }}>
+            <h1 style={{ textAlign: 'center' }}>Create Shop</h1>
+            <Snackbar
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'center',
+              }}
+              open={open}
+              autoHideDuration={6000}
+              onClose={this.handleClose}
             >
-              Create
-            </Button>
-          </Paper>
-        </div>
-      </React.Fragment>
+              {this.renderSnackBar(snackItems, 'success')}
+            </Snackbar>
+            <Paper elevation={1} className={classes.paper}>
+              <TextField
+                onChange={this.handleInputChange}
+                id="name"
+                name="name"
+                label="Shop Name"
+                fullWidth
+                value={name}
+              />
+              <TextField
+                onChange={this.handleInputChange}
+                id="category"
+                name="category"
+                label="Category"
+                fullWidth
+                value={category}
+              />
+              <TextField
+                onChange={this.handleInputChange}
+                id="description"
+                name="description"
+                label="Description"
+                fullWidth
+                rows={5}
+                multiline
+                value={description}
+              />
+              <Button
+                onClick={() => this.formSubmit(createShopFunction, error)}
+                disabled={loading}
+                type="submit"
+                fullWidth
+                variant="raised"
+                color="primary"
+              >
+                Create
+              </Button>
+            </Paper>
+          </div>
+        )}
+      </Mutation>
     );
   }
 }
 
-export default withStyles(styles)(CreateShop);
+export default withStyles(styles)(CreateShopPage);
